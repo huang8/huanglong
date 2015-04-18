535f3a7ab8c2948e793274a891614f15b253cca3
Your Name <13717314275@163.com>
2015-04-18 23:22:08 +0800

    hello

---
+++ tz3.php
@@ -0,0 +1,7 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=fkcp';
+$pdo = new PDO($dsn,'root','');
+$sql = 'select * from fk_goods limit 10';
+$stmt = $pdo->query($sql);
+$list = $stmt->fetchAll(PDO::FETCH_ASSOC);
+var_dump($list);
